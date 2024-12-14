380e2bc3bf84b559cdadb099a2ca9977cbf21c70
youssef hataba <joohataba@gmail.com>
2024-12-14 02:45:35 +0200

    add paginatin

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,6 +24,20 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    //? Pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;;
+    
+    if(req.query.page){
+      const numTours = await Tour.countDocuments();
+      if(skip >= numTours){
+        throw new Error('This Page Does Not Exist!');
+      }
+    }
+
+    query = query.skip(skip).limit(limit)
+
     const tours = await query;
 
     res.status(200).json({
