5835c047cb2b1a5d4448a353502982aa48d27d77
youssef hataba <joohataba@gmail.com>
2024-12-25 13:46:44 +0200

    add error handling

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({path: "./config.env"});
-const app = require("./app"); 
+const app = require("./app");
 
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 
@@ -14,7 +14,6 @@ mongoose
   .catch((err) => {
     console.error("DB connection failed:", err.message);
   });
-
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
