6b94e7be107e2b92b3fa5502c3a1f6bde4015ad9
youssef hataba <joohataba@gmail.com>
2024-12-31 22:15:57 +0200

    Protecting Tour Routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,22 +66,26 @@ exports.login = async (req, res, next) => {
 };
 
 exports.protect = async (req, res, next) => {
-  //? 1) Getting token and check of it's there
-  let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-    token = req.headers.authorization.split(" ")[1];
+  try {
+    //? 1) Getting token and check of it's there
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return next(new AppError("Please log in to get access to the tours", 401));
+    }
+
+    //? 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    console.log(decoded);
+
+    //? 3) Check if user still exists
+
+    //? 4) check if user changed password after the token was issued
+  } catch (err) {
+    return next(new AppError("Invalid token, please login again", 401));
   }
-
-  if (!token) {
-    return next(new AppError("Please log in to get access to the tours", 401));
-  }
-
-  //? 2) Verification token
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-  //? 3) Check if user still exists
-
-  //? 4) check if user changed password after the token was issued
-
   next();
 };
