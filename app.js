c386157f97566574c1613b48125e74ee5103b2c4
youssef hataba <joohataba@gmail.com>
2025-01-25 16:55:40 +0200

    add global middleware to protect from DDoS attacks or spam

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
@@ -8,9 +10,18 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+// 1) Global Middlewares
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 200, // 200 request with the same ip address
+  windowMs: 60 * 60 * 1000, // in one hour
+  message: "Too many requests from this IP, please try again in an hour.",
+});
+
+app.use("/api", limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -24,16 +35,15 @@ app.use((req, res, next) => {
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
 
-app.all('*',(req,res,next) => {
+app.all("*", (req, res, next) => {
   // const err = new Error(`Can't find ${req.originalUrl} on this server.`);
   // err.status = 'fail';
   // err.statusCode = 404;
 
-  next(new AppError(`Can't find ${req.originalUrl} on this server.`,404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
 });
 
-
 //? Error handling middleware
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 module.exports = app;
