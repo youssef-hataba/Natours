6e13ae46a59ca545ba0d763267f1366a63c41cb7
youssef hataba <joohataba@gmail.com>
2025-02-03 23:39:00 +0200

    resizing images using sharp

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,50 @@
 const multer = require("multer");
+const sharp = require("sharp");
 const User = require("../models/userModel");
 const AppError = require("../utils/appError");
 const handlerFactory = require("./handlerFactory");
+const {json} = require("express");
 
-const multerStorage = multer.diskStorage({
-  destination:(req,file,cb)=>{
-    cb(null,'public/img/users')
-  },
-  filename:(req,file,cb)=>{
-    const ext = file.mimetype.split('/')[1];
-    cb(null,`user-${req.user.id}-${Date.now()}.${ext}`);
-  }
-});
+// const multerStorage = multer.diskStorage({
+//   destination:(req,file,cb)=>{
+//     cb(null,'public/img/users')
+//   },
+//   filename:(req,file,cb)=>{
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null,`user-${req.user.id}-${Date.now()}.${ext}`);
+//   }
+// });
 
-const multerFilter = (req,file,cb)=>{
-  if(file.mimetype.startsWith('image')) cb(null,true);
-  else cb(new AppError("Only images are allowed",400),false);
-}
+
+// if you want to make some image processing -> use multer.memoryStorage not multer.diskStorage;
+const multerStorage = multer.memoryStorage({});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) cb(null, true);
+  else cb(new AppError("Only images are allowed", 400), false);
+};
 
 const upload = multer({
-  storage:multerStorage,
-  fileFilter:multerFilter,
+  storage: multerStorage,
+  fileFilter: multerFilter,
 });
 
 exports.uploadUserPhoto = upload.single("photo");
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  //? 2) resize the image using Sharp
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat("jpeg")
+    .jpeg({quality: 90})
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next(); 
+};
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -67,7 +88,7 @@ exports.updateMe = async (req, res, next) => {
 
   //? 2) Filter out unwanted fields from req.body
   const filteredBody = filterObj(req.body, "name", "email");
-  if(req.file) filteredBody.photo = req.file.filename;
+  if (req.file) filteredBody.photo = req.file.filename;
 
   //? 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,7 @@ router.use(authController.protect);
 
 router.patch("/updateMyPassword", authController.updatePassword);
 router.get("/me", userController.getMe, userController.getUser);
-router.patch("/updateMe", userController.uploadUserPhoto, userController.updateMe);
+router.patch("/updateMe", userController.uploadUserPhoto,userController.resizeUserPhoto, userController.updateMe);
 router.delete("/deleteMe", userController.deleteMe);
 
 // routes for adminstrators
